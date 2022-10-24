01385fd728b270bef3e0540b6e5e099e0c2b08f5
jeongho-kang <aj9037@naver.com>
2022-10-24 22:28:09 +0900

    first commit

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <>
+            <h1>footer 입니다.</h1>
+        </>
+    );
+}
---
+++ pages/project.js
@@ -0,0 +1,11 @@
+import Layout from "../components/layout";
+
+export default function Project() {
+    return(
+        <>
+            <Layout>
+            <h1>project</h1>
+            </Layout>
+        </>
+    )
+}
